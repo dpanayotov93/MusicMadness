e453d599f290d52a9023040005d9926c649b8e91
dpanayotov93 <dpanayotov93@gmail.com>
2017-08-29 13:50:50 +0000

    Base  level design done. Base playr controls done. Needs audio decoding, bar animations, player shooting, physics for the bars and enemies (+AI).

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 // Global variables
 var $ = document.querySelector.bind(document),
     $$ = document.querySelectorAll.bind(document),
-    canvas, ctx, player;
+    keys= {},
+    canvas, ctx, player, level;
 
 window.onload = function() {
     // Setting global variables
@@ -67,22 +68,176 @@ function showMainMenu() {
 
 function init() {
     if(!isPointerLocked()) {
-        console.info('--GAME STARTED--');
-        player = new Player();
+        console.info('--GAME STARTING--');
         canvas.removeEventListener('click',init, false );
         ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas from the text
-        addGUI();
+        
+        ctx.fillText("Loading...", canvas.width/2, canvas.height/2);
+        
+        // Setup Player
+        player = new Player(); 
+        player.sprite.src = player.spriteURL.right; // Add sprite
+        player.sprite.onload = function() {
+            level = new Level(); // Create level
+            level.init();
+        };
     }
 }
 
-function addGUI() {
+function updateGUI() {
     ctx.font = "20px Arial";
-    ctx.textAlign = "left";
-    ctx.fillText("Health: 100/100", 10, canvas.height - 50);
-    ctx.fillText("Ammo: " + player.curHealth + "/" + player.maxHealth, 10, canvas.height - 25);
+    ctx.fillStyle = "#ffffff";
+    ctx.textAlign = "center";
+    ctx.fillText("Health: " + player.curHealth + "/" + player.maxHealth, canvas.width / 2, 25);
+    ctx.fillText("Ammo: " + player.curAmmo + "/" + player.maxAmmo, canvas.width / 2, 50);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    // console.log('--UPDATED--');
+    player.checkControls();
+    if(player.position.y === player.spawnPoint.y && !keys[87]) {
+        player.isJumping = false;
+    }
+}
+
+function onKeyDown(e) {
+    var key = e.which;
+    // console.log(key);
+    keys[key] = true;
+}
+
+function onKeyUp(e) {
+    var key = e.which;
+    keys[key] = false;
 }
 
 var Player = function() {
+    this.width = 200;
+    this.height = 250;
+    this.spawnPoint = {
+        x: 50,
+        y: canvas.height - 500
+    }
     this.maxHealth = 100;
     this.curHealth = 100;
+    this.maxAmmo = 100;
+    this.curAmmo = 100;
+    this.speed = 10;
+    this.gravity = 250;
+    this.isJumping = false;
+    this.jumpHeight = 300;
+    this.spriteURL = {
+        right: 'assets/player-right.png',
+        left: 'assets/player-left.png'
+        
+    }
+    this.sprite = new Image();
+    this.position = {
+        x: 0,
+        y: 0
+    }
+
+    this.draw = function(position) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas from the text
+        ctx.drawImage(this.sprite, position.x, position.y, this.width, this.height);
+        this.position.x = position.x;
+        this.position.y = position.y;
+        
+        level.floor.draw(); // Draw the floor
+        updateGUI(); // Draw the GUI                   
+    }
+    
+    this.checkControls = function() {
+        if(keys[65]) this.moveLeft(); // A - Move Left
+        if(keys[68]) this.moveRight(); // D - Move Right
+        if(!this.isJumping) {
+            if(keys[87]) this.jump(); // W - Jump
+        }
+    }
+    
+    this.moveLeft = function() {
+        console.log('moving left');
+        var movePosition = {
+            x: this.position.x - this.speed ,
+            y: this.position.y
+        }
+        if(movePosition.x < -20*this.speed) movePosition.x = canvas.width - this.speed;
+        this.draw(movePosition);
+    }
+    
+    this.moveRight = function() {
+        console.log('moving right');
+        var movePosition = {
+            x: this.position.x + this.speed ,
+            y: this.position.y
+        }
+        if(movePosition.x > canvas.width) movePosition.x = this.speed;
+        this.draw(movePosition);
+    }
+    
+    this.jump = function() {
+        if(!this.isJumping) {
+            this.isJumping = true;
+            
+            var movePosition = {
+                x: this.position.x,
+                y: this.position.y - this.jumpHeight
+            }
+            this.draw(movePosition);
+            
+            setTimeout(function() {
+                var movePosition = {
+                    x: player.position.x,
+                    y: player.spawnPoint.y
+                }
+                player.draw(movePosition);
+            }, 100)
+        }
+    }
 }
+
+var Level = function() {
+    this.init = function() {
+        console.info('--DRAWING LEVEL--');
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas from the text
+
+        // Get the player spawn position
+        var drawPosition = {
+            x: player.spawnPoint.x,
+            y: player.spawnPoint.y
+        };
+        player.draw(drawPosition); // Draw the player
+        
+        this.floor.draw(); // Draw the floor
+        updateGUI(); // Draw the GUI
+        
+        // Add keypress event listeners
+        window.addEventListener('keydown', onKeyDown, false);
+        window.addEventListener('keyup', onKeyUp, false);
+        
+        animate(); // Start the update cycle
+    };
+    
+    this.floor = {
+        width: canvas.width,
+        height: 250,
+        segmentWidth: canvas.width / 8,
+        segmentsN: canvas.width / 240, // TODO: Change num to segmentWidth prop
+        startX: 0,
+        startY: canvas.height - 250, // TODO: Change num to height prop
+        color: '#333333',
+        draw: function() { // TODO: Pass bar levels array from detected audio beats and adjust the height of the bars according to them
+            ctx.fillStyle = level.floor.color; // Set the base floor color
+            ctx.fillRect(level.floor.startX, level.floor.startY, level.floor.width, level.floor.height); // Set the size and position of the floor
+            for(var i = 0; i < level.floor.segmentsN; i += 1) {
+                ctx.fillStyle = testColors[i]; // Set the current bar color
+                ctx.fillRect(level.floor.startX + level.floor.segmentWidth*i, level.floor.startY + 10*i, level.floor.segmentWidth, level.floor.height + 10*i); // Set the size and position of the current bar
+            }
+        }
+    };
+
+}
+
+
+var testColors = ['cyan', 'blue', 'lime', 'green', 'yellow', 'orange', 'red', 'magenta'];
